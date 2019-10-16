1e3cca4b0184d936c179b54c09eb79741c9be127
abel <abelcarreras83@gmail.com>
2019-10-16 23:35:18 +0200

    cleaning structure

--- kimonet/analysis/__init__.py
+++ kimonet/analysis/__init__.py
@@ -1,7 +1,6 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from kimonet import all_none
 from scipy import stats
 from mpl_toolkits.mplot3d import Axes3D  # Actually needed
 
@@ -178,6 +177,24 @@ class Trajectory:
             return self.trajectory['time'][-1]
         else:
             return 0
+
+
+def all_none(centre_list):
+    """
+    :param centre_list:
+    :return: checks if all positions in a list are None
+    """
+    none_yes = 1
+
+    for element in centre_list:
+
+        if element is None:
+            none_yes = none_yes * 1
+
+        else:
+            none_yes = none_yes * 0
+
+    return bool(none_yes)
 
 
 def merge_json_files(file1, file2):
