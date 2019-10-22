766f26efd7f6f5cf4a86b4c33088fa73178be378
abel <abelcarreras83@gmail.com>
2019-10-22 18:11:17 +0200

    Fixed periodic problem in the simulation when used multiple excitons Remade analysis functions to work with multiple excitation states minor cleaning

--- scripts/launch_script.py
+++ scripts/launch_script.py
@@ -2,8 +2,6 @@ from kimonet.system import ordered_system, disordered_system
 from kimonet.core import update_system
 from kimonet.analysis import Trajectory, visualize_system, TrajectoryAnalysis
 from kimonet.molecules import Molecule
-import json
-import warnings
 import numpy as np
 np.random.seed(0)
 
@@ -18,11 +16,13 @@ All energies must be given in eV. By default initialized at gs.
 
 # excitation energies of the electronic states (eV)
 state_energies = {'gs': 0,
-                  's1': 1}
+                  's1': 1,
+                  's2': 2}
 
 # reorganization energies of the states (eV)
 reorganization_energies = {'gs': 0,
-                           's1': 0.2}
+                           's1': 0.2,
+                           's2': 0.1}
 
 molecule = Molecule(state_energies=state_energies,
                     reorganization_energies=reorganization_energies,
@@ -43,7 +43,7 @@ max_steps = 100000                              # maximum number of steps for tr
 
 system = ordered_system(conditions=conditions,
                         molecule=molecule,
-                        lattice={'size': [3, 3], 'parameters': [3.0, 3.0]},  # Angstroms
+                        lattice={'size': [10, 10], 'parameters': [3.0, 3.0]},  # Angstroms
                         orientation=[0, 0, 0])
 
 # visualize_system(system)
@@ -53,6 +53,7 @@ for j in range(num_trajectories):
 
     system.add_excitation_center('s1')
     # system.add_excitation_index('s1', 0)
+    # system.add_excitation_random('s2', 20)
 
     # visualize_system(system)
 
@@ -71,34 +72,19 @@ for j in range(num_trajectories):
         # visualize_system(system)
 
         if i == max_steps-1:
-            warnings.warn('Maximum number of steps reached!!')
+            print('Maximum number of steps reached!!')
 
     system.reset()
 
     trajectories.append(trajectory)
 
-z = 2
 
 # diffusion properties
-d_tensor = np.average([traj.get_diffusion_tensor(0) for traj in trajectories], axis=0)
-print('diffusion tensor')
-print(d_tensor)
-
-d_coefficient = np.average([traj.get_diffusion(0) for traj in trajectories])
-lifetime = np.average([traj.get_lifetime(0) for traj in trajectories])
-print('diffusion coefficient (average): {}angs^2/ns'.format(d_coefficient))
-print('lifetime: {} ns'.format(lifetime))
-
-length2 = np.average([traj.get_diffusion_length_square(0) for traj in trajectories])
-print('diffusion length: ', np.sqrt(length2), np.sqrt(2 * z * d_coefficient * lifetime), 'angs')
-print('diffusion(2)', length2 / (lifetime * 2 * z))
-
-# print('--------------TEST2--------------')
 analysis = TrajectoryAnalysis(trajectories)
 
 print(analysis)
 
-print('diffusion coefficient (average): {}angs^2/ns'.format(analysis.diffusion_coefficient()))
+print('diffusion coefficient (average): {} angs^2/ns'.format(analysis.diffusion_coefficient()))
 print('lifetime: {} ns'.format(analysis.lifetime()))
 print('diffusion length: {} angs'.format(analysis.diffusion_length()))
 
@@ -108,7 +94,6 @@ print('diffusion length tensor')
 print(analysis.diffusion_length_tensor())
 # print(np.sqrt(analysis.diffusion_coeff_tensor()*analysis.lifetime()*2))
 
-
 plt = analysis.plot_2d()
 plt.figure()
 analysis.plot_distances()
@@ -116,34 +101,6 @@ analysis.plot_distances()
 plt.show()
 
 exit()
-
-
-#######################################################################################################################
-
-output_file_name = '2_excitons_simulation_1d.json'      # name of the output file where the trajectories will be saved
-                                                        # .json format
-
-#######################################################################################################################
-
-
-# We collect all the outputs in a dictionary system_information and write it in the output_file
-
-system_information = {'conditions': system['conditions'],
-                      'lattice': system['lattice'],
-                      'orientation': 'parallel',
-                      'type': system['type'],
-                      'excitons': {'s1': ['centre']},
-                      'state_energies': state_energies,
-                      'reorganization_energies': reorganization_energies,
-                      'transition_moment': list(molecule.transition_moment)}
-
-output = {'system_information': system_information,
-          'trajectories': [traj for traj in trajectories],
-          'max_steps': max_steps}
-
-with open(output_file_name, 'w') as write_file:
-    json.dump(output, write_file)
-
 
 ################# Parallel test (py3 only) ##################
 
