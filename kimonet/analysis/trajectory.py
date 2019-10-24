Implemented averaged properties calculation methods for trajectory analysis other small fixes

kimonet/analysis/trajectory.py
@@ -23,7 +23,7 @@ class Trajectory:
 
         self.times = [0]
         self.process = []
-        self.exciton_altered = []
+        # self.exciton_altered = []
         self.supercell = system.supercell
         self.system = system
 
@@ -33,6 +33,11 @@ class Trajectory:
 
         for i, center in enumerate(self.system.centers):
             self.center_track['{}'.format(center)] = i
+
+        self.states = set()
+        for center in self.centers:
+            for state in center['state']:
+                self.states.add(state)
 
         self.n_dim = len(self.centers[0]['coordinates'][0])
         self.n_centers = len(self.centers)
@@ -49,7 +54,7 @@ class Trajectory:
         ordered_points = np.sort(np.array(self.labels[label], dtype=dtype), order='center')
         continuous_sections = np.split(ordered_points, np.where(np.diff(ordered_points['position']) != 1)[0] + 1)
         if len(ordered_points) == 0:
-            return [(np.array((0, 0), dtype=dtype), np.array((0, 0), dtype=dtype))]
+            return [(np.array((0, 0), dtype=dtype), np.array((0, -1), dtype=dtype))]
         return [(seq[0], seq[-1]) for seq in continuous_sections]
 
     def add(self, change_step, time_step):
@@ -61,7 +66,6 @@ class Trajectory:
         """
 
         self.times.append(self.times[-1] + time_step)
-        self.exciton_altered.append(change_step['index'])
         self.process.append(change_step['process'])
 
         # key = next(key for key, value in self.center_track.items() if value == change_step['donor'])
@@ -81,9 +85,14 @@ class Trajectory:
             self.centers[i]['state'].append(excited_state)
             self.centers[i]['cell_state'].append(cell_state)
 
+            self.states.add(excited_state)
+
         # print(self.centers[0]['cell_state'][-1])
         # print('t:', len(self.times), len(self.centers[0]['state']))
         return
+
+    def get_states(self):
+        return self.states
 
     def get_number_of_centers(self):
         return self.n_centers
@@ -206,6 +215,25 @@ class Trajectory:
             t.append(self.times[fin['position']+1] - self.times[ini['position']])
 
         return np.average(t)
+
+    def get_lifetime_ratio(self, state):
+
+        t_tot = 0
+        for center in self.centers:
+            try:
+                t_tot += self.times[len(center['state'])]
+            except IndexError:
+                t_tot += self.times[-1]
+
+        t = 0
+        sections = self.get_ranges_from_label(state)
+        for ini, fin in sections:
+            try:
+                t += self.times[fin['position']+1] - self.times[ini['position']]
+            except IndexError:
+                t += self.times[fin['position']] - self.times[ini['position']]
+
+        return t/t_tot
 
     def get_diffusion_length_square(self, state):
 

kimonet/analysis/trajectory_analysis.py
@@ -10,6 +10,11 @@ class TrajectoryAnalysis:
         self.n_dim = trajectories[0].get_dimension()
         self.n_traj = len(trajectories)
 
+        self.states = set()
+        for traj in trajectories:
+            self.states |= traj.get_states()
+        self.states = self.states
+
     def __str__(self):
 
         txt_data = '\nTrajectory Analysis\n'
@@ -17,10 +22,17 @@ class TrajectoryAnalysis:
         txt_data += 'Number of trajectories: {}\n'.format(self.n_traj)
         txt_data += 'Dimension: {}\n'.format(self.n_dim)
         txt_data += 'Number of centers: {}\n'.format(self.n_centers)
+        txt_data += 'States: {}\n'.format(self.states)
 
         return txt_data
 
-    def diffusion_coeff_tensor(self):
+    def get_states(self):
+        return self.states
+
+    def get_lifetime_ratio(self, state):
+        return np.average([traj.get_lifetime_ratio(state) for traj in self.trajectories])
+
+    def diffusion_coeff_tensor(self, state):
         """
         calculate the average diffusion tensor defined as:
 
@@ -29,9 +41,9 @@ class TrajectoryAnalysis:
         :param trajectories: list of Trajectory
         :return:
         """
-        return np.nanmean([traj.get_diffusion_tensor('s1') for traj in self.trajectories], axis=0)
+        return np.nanmean([traj.get_diffusion_tensor(state) for traj in self.trajectories], axis=0)
 
-    def diffusion_length_tensor(self):
+    def diffusion_length_tensor(self, state):
         """
         calculate the average diffusion length tensor defined as:
 
@@ -40,11 +52,11 @@ class TrajectoryAnalysis:
         :param trajectories: list of Trajectory
         :return:
         """
-        dl_tensor = np.average([traj.get_diffusion_length_square_tensor('s1') for traj in self.trajectories], axis=0)
+        dl_tensor = np.average([traj.get_diffusion_length_square_tensor(state) for traj in self.trajectories], axis=0)
 
         return np.sqrt(np.abs(dl_tensor))
 
-    def diffusion_coefficient(self):
+    def diffusion_coefficient(self, state=None):
         """
         Return the average diffusion coefficient defined as:
 
@@ -52,12 +64,30 @@ class TrajectoryAnalysis:
 
         :return:
         """
-        return np.nanmean([traj.get_diffusion('s1') for traj in self.trajectories])
 
-    def lifetime(self):
-        return np.average([traj.get_lifetime('s1') for traj in self.trajectories])
+        sum_diff = 0
+        if state is None:
+            for s in self.get_states():
+                diffusion_list = [traj.get_diffusion(s) for traj in self.trajectories]
+                if not np.isnan(diffusion_list).all():
+                    sum_diff += np.nanmean(diffusion_list) * self.get_lifetime_ratio(s)
+            return sum_diff
 
-    def diffusion_length(self):
+        return np.nanmean([traj.get_diffusion(state) for traj in self.trajectories])
+
+    def lifetime(self, state=None):
+
+        sum_diff = 0
+        if state is None:
+            for s in self.get_states():
+                diffusion_list = [traj.get_lifetime(s) for traj in self.trajectories]
+                if not np.isnan(diffusion_list).all():
+                    sum_diff += np.nanmean(diffusion_list) * self.get_lifetime_ratio(s)
+            return sum_diff
+
+        return np.average([traj.get_lifetime(state) for traj in self.trajectories])
+
+    def diffusion_length(self, state=None):
         """
         Return the average diffusion coefficient defined as:
 
@@ -65,7 +95,15 @@ class TrajectoryAnalysis:
 
         :return:
         """
-        length2 = np.nanmean([traj.get_diffusion_length_square('s1') for traj in self.trajectories])
+        sum_diff = 0
+        if state is None:
+            for s in self.get_states():
+                diffusion_list = [traj.get_diffusion_length_square(s) for traj in self.trajectories]
+                if not np.isnan(diffusion_list).all():
+                    sum_diff += np.nanmean(diffusion_list) * self.get_lifetime_ratio(s)
+            return np.sqrt(sum_diff)
+
+        length2 = np.nanmean([traj.get_diffusion_length_square(state) for traj in self.trajectories])
         return np.sqrt(length2)
 
     def plot_2d(self):

kimonet/core/processes/__init__.py
@@ -2,58 +2,42 @@ import numpy as np
 from kimonet.core.processes.coupling import functions_dict
 from kimonet.core.processes.fcwd import marcus_fcwd
 
-decay_data = {}
 
-
-def get_processes_and_rates(centre, system, i):
+def get_processes_and_rates(centre, system):
     """
-    :param i: index of the exciton
     :param centre: Index of the studied excited molecule (Donor)
     :param system: Instance of System class
     Computes the transfer and decay rates and builds two dictionaries:
             One with the decay process as key and its rate as argument
             One with the transferred molecule index as key and {'process': rate} as argument
 
-    :return:    process_list: List of elements like dict(center, process, new molecule)
+    :return:    process_list: List of processes in named tuple format
                 rate_list: List with the respective rates
-                The list indexes coincide.
     """
 
-    transfer_processes, transfer_rates = get_transfer_rates(centre, system, i)
-    # calls an external function that computes the transfer rates for all possible transfer processes between
-    # the centre and all its neighbours
+    transfer_processes, transfer_rates = get_transfer_rates(centre, system)
 
-    decay_processes, decay_rates = get_decay_rates(centre, system, i)
-    # calls an external function that computes the decay rates for all possible decay processes of the centre.
-    # Uses a method of the class Molecule
+    decay_processes, decay_rates = get_decay_rates(centre, system)
 
-    # merges all processes in a list and the same for the rates
-    # the indexes of the list must coincide (same length. rate 'i' is related to process 'i')
+    # merges all processes & rates
     process_list = decay_processes + transfer_processes
     rate_list = decay_rates + transfer_rates
 
     return process_list, rate_list
 
 
-def get_transfer_rates(centre, system, exciton_index):
+def get_transfer_rates(center, system):
     """
-    :param centre: Index of the studies excited molecule
+    :param center: Index of the studies excited molecule
     :param system: Dictionary with the list of molecules and additional physical information
-    :param exciton_index
     :return: Two lists, one with the transfer rates and the other with the transfer processes.
-    For each possible acceptor in neighbour_indexes computes the transfer rate using the Fermi's Golden Rule:
-        - For the spectral overlap the Marcus Formula is used for all cases.
-        - For the electronic coupling an external dictionary is defined. It contains the possible couplings between
-            two states (more than one allowed). The keys of this dictionary are like:
-                'state1_state2' + str(additional information)
-            If the key 'state1_state2' is not in the dictionary the electronic coupling shall be taken as 0.
     """
 
-    neighbour_indexes, cell_increment = system.get_neighbours(centre)
+    neighbour_indexes, cell_increment = system.get_neighbours(center)
 
     conditions = system.conditions           # physical conditions of the system
 
-    donor = system.molecules[centre]         # excited molecule
+    donor = system.molecules[center]         # excited molecule
 
     transfer_rates = []                         # list that collects the transfer rates (only the numerical values)
     transfer_processes = []                     # list that collects the transfer processes dict(donor,process,acceptor)
@@ -71,21 +55,20 @@ def get_transfer_rates(centre, system, exciton_index):
             e_coupling = coupling_function(donor, acceptor, conditions, system.supercell)
             transfer_rates.append(2*np.pi * e_coupling**2 * spectral_overlap)  # Fermi's Golden Rule
 
-            transfer_processes.append({'donor': int(centre), 'process': process, 'acceptor': int(neighbour),
-                                       'index': exciton_index, 'cell_increment': cell_incr})
+            transfer_processes.append({'donor': int(center), 'process': process, 'acceptor': int(neighbour),
+                                       'cell_increment': cell_incr})
 
     return transfer_processes, transfer_rates
 
 
-def get_decay_rates(centre, system, exciton_index):
+def get_decay_rates(center, system):
     """
-    :param centre: index of the excited molecule
+    :param center: index of the excited molecule
     :param system: Dictionary with all the information of the system
-    :param exciton_index
     :return: A dictionary with the possible decay rates
     For computing them the method get_decay_rates of class molecule is call.
     """
-    donor = system.molecules[centre]
+    donor = system.molecules[center]
 
     decay_processes = []            # list of decays processes: dicts(donor, process, acceptor)
     decay_rates = []                # list of the decay rates (numerical values)
@@ -94,19 +77,15 @@ def get_decay_rates(centre, system, exciton_index):
 
     # splits the dictionary in two lists
     for key in decay_complete:
-        decay_processes.append({'donor': centre, 'process': key, 'acceptor': centre, 'index': exciton_index})
+        decay_processes.append({'donor': center, 'process': key, 'acceptor': center})
         decay_rates.append(decay_complete[key])
-
-    # the process include: the index of the donor (in molecules), the key of the process,
-    # the index of the acceptor (in molecules) and the index of the exciton (in centres).
-    # This last parameter acts as the name of the exciton
 
     return decay_processes, decay_rates
 
 
 def get_allowed_processes(donor, acceptor):
     """
-    Get the allowed processes given donor and acceptor
+    Get the allowed processes for a given donor and acceptor
 
     :param donor: Molecule class instance
     :param acceptor: Molecule class instance

kimonet/tests/__init__.py
@@ -71,8 +71,8 @@ class TestKimonet(unittest.TestCase):
         test = {'diffusion coefficient': np.around(analysis.diffusion_coefficient(), decimals=6),
                 'lifetime': np.around(analysis.lifetime(), decimals=6),
                 'diffusion length': np.around(analysis.diffusion_length(), decimals=6),
-                'diffusion tensor': np.around(analysis.diffusion_coeff_tensor(), decimals=6).tolist(),
-                'diffusion length tensor': np.around(analysis.diffusion_length_tensor(), decimals=6).tolist()
+                'diffusion tensor': np.around(analysis.diffusion_coeff_tensor('s1'), decimals=6).tolist(),
+                'diffusion length tensor': np.around(analysis.diffusion_length_tensor('s1'), decimals=6).tolist()
                 }
 
         print(test)
@@ -91,8 +91,8 @@ class TestKimonet(unittest.TestCase):
         from kimonet.core.processes import get_transfer_rates, get_decay_rates
 
         self.system.add_excitation_index('s1', 0)
-        transfer_x, _, transfer_y, _ = get_transfer_rates(0, self.system, 0)[1]
-        decay, = get_decay_rates(0, self.system, 0)[1]
+        transfer_x, _, transfer_y, _ = get_transfer_rates(0, self.system)[1]
+        decay, = get_decay_rates(0, self.system)[1]
 
         print('analytical model')
         print('----------------')

scripts/launch_script.py
@@ -16,11 +16,13 @@ All energies must be given in eV. By default initialized at gs.
 
 # excitation energies of the electronic states (eV)
 state_energies = {'gs': 0,
-                  's1': 1}
+                  's1': 1,
+                  's2': 1}
 
 # reorganization energies of the states (eV)
 reorganization_energies = {'gs': 0,
-                           's1': 0.2}
+                           's1': 0.2,
+                           's2': 0.2}
 
 molecule = Molecule(state_energies=state_energies,
                     reorganization_energies=reorganization_energies,
@@ -36,7 +38,7 @@ conditions = {'temperature': 273.15,            # temperature of the system (K)
 
 #######################################################################################################################
 
-num_trajectories = 500                          # number of trajectories that will be simulated
+num_trajectories = 50                          # number of trajectories that will be simulated
 max_steps = 100000                              # maximum number of steps for trajectory allowed
 
 system = ordered_system(conditions=conditions,
@@ -51,7 +53,7 @@ for j in range(num_trajectories):
 
     system.add_excitation_center('s1')
     # system.add_excitation_index('s1', 0)
-    # system.add_excitation_random('s2', 20)
+    # system.add_excitation_random('s2', 3)
 
     # visualize_system(system)
 
@@ -73,6 +75,9 @@ for j in range(num_trajectories):
 
     system.reset()
 
+    # print(trajectory.get_lifetime_ratio('s1'), trajectory.get_lifetime_ratio('s2'), trajectory.get_lifetime_ratio('s3'))
+    # print(trajectory.get_lifetime_ratio('s3'))
+
     trajectories.append(trajectory)
 
 
@@ -82,13 +87,15 @@ analysis = TrajectoryAnalysis(trajectories)
 print(analysis)
 
 print('diffusion coefficient (average): {} angs^2/ns'.format(analysis.diffusion_coefficient()))
+print('diffusion coefficient (s1): {} angs^2/ns'.format(analysis.diffusion_coefficient('s1')))
+
 print('lifetime: {} ns'.format(analysis.lifetime()))
 print('diffusion length: {} angs'.format(analysis.diffusion_length()))
 
 print('diffusion tensor')
-print(analysis.diffusion_coeff_tensor())
+print(analysis.diffusion_coeff_tensor('s1'))
 print('diffusion length tensor')
-print(analysis.diffusion_length_tensor())
+print(analysis.diffusion_length_tensor('s1'))
 # print(np.sqrt(analysis.diffusion_coeff_tensor()*analysis.lifetime()*2))
 
 plt = analysis.plot_2d()

scripts/parallel_run.py
@@ -59,7 +59,7 @@ def run_trajectory(system, index):
 
 
 # executor = futures.ThreadPoolExecutor(max_workers=4)
-executor = futures.ProcessPoolExecutor(max_workers=4)
+executor = futures.ProcessPoolExecutor(max_workers=14)
 
 futures_list = []
 for i in range(num_trajectories):
@@ -77,9 +77,9 @@ print('lifetime: {} ns'.format(analysis.lifetime()))
 print('diffusion length: {} angs'.format(analysis.diffusion_length()))
 
 print('diffusion tensor')
-print(analysis.diffusion_coeff_tensor())
+print(analysis.diffusion_coeff_tensor('s1'))
 print('diffusion length tensor')
-print(analysis.diffusion_length_tensor())
+print(analysis.diffusion_length_tensor('s1'))
 # print(np.sqrt(analysis.diffusion_coeff_tensor()*analysis.lifetime()*2))
 
 plt = analysis.plot_2d()