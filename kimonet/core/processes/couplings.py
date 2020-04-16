ac5dff54e3a42d3c29e67a512da7e0634f147a42
abel <abelcarreras83@gmail.com>
2020-04-16 20:27:29 +0200

    Added new function for foster coupling calculation

--- kimonet/core/processes/couplings.py
+++ kimonet/core/processes/couplings.py
@@ -28,8 +28,8 @@ def forster_coupling(donor, acceptor, conditions, supercell):
     if hash_string in foster_data:
         return foster_data[hash_string]
 
-    mu_d = donor.get_transition_moment(to_state='gs')                     # transition dipole moment (donor) a.u
-    mu_a = acceptor.get_transition_moment(to_state=donor.state)                  # transition dipole moment (acceptor) a.u
+    mu_d = donor.get_transition_moment(to_state='gs')            # transition dipole moment (donor) e*angs
+    mu_a = acceptor.get_transition_moment(to_state=donor.state)  # transition dipole moment (acceptor) e*angs
 
     r_vector = intermolecular_vector(donor, acceptor)       # position vector between donor and acceptor
     r_vector, _ = minimum_distance_vector(r_vector, supercell)
@@ -47,6 +47,58 @@ def forster_coupling(donor, acceptor, conditions, supercell):
     foster_data[hash_string] = forster_coupling                            # memory update for new couplings
 
     # print('f:', forster_coupling, distance)
+    return forster_coupling
+
+
+def forster_coupling_extended(donor, acceptor, conditions, supercell, longitude=1, n_divisions=1):
+    """
+    Compute Forster coupling in eV
+
+    :param donor: excited molecules. Donor
+    :param acceptor: neighbouring molecule. Possible acceptor
+    :param conditions: dictionary with physical conditions
+    :param supercell: the supercell of the system
+    :return: Forster coupling
+    """
+
+    function_name = inspect.currentframe().f_code.co_name
+
+    # donor <-> acceptor interaction symmetry
+    hash_string = str(hash((donor, function_name)) + hash((acceptor, function_name)))
+    # hash_string = str(hash((donor, acceptor, function_name))) # No symmetry
+
+    if hash_string in foster_data:
+        return foster_data[hash_string]
+
+    mu_d = donor.get_transition_moment(to_state='gs')              # transition dipole moment (donor) e*angs
+    mu_a = acceptor.get_transition_moment(to_state=donor.state)    # transition dipole moment (acceptor) e*angs
+
+    mu_ai = mu_a / n_divisions
+    mu_di = mu_d / n_divisions
+
+    n = conditions['refractive_index']                      # refractive index of the material
+
+    r_vector = intermolecular_vector(donor, acceptor)  # position vector between donor and acceptor
+    r_vector, _ = minimum_distance_vector(r_vector, supercell)
+
+    k_e = 1.0 / (4.0 * np.pi * VAC_PERMITTIVITY)
+
+    forster_coupling = 0
+    for x in np.linspace(-1 + 1/n_divisions, 1 - 1/n_divisions, n_divisions):
+        for y in np.linspace(-1 + 1 / n_divisions, 1 - 1 / n_divisions, n_divisions):
+
+            dr_a = mu_a / np.linalg.norm(mu_a) * longitude * x
+            dr_d = mu_d / np.linalg.norm(mu_d) * longitude * y
+            r_vector_i = r_vector + dr_a + dr_d
+
+            distance = np.linalg.norm(r_vector_i)
+
+            k = orientation_factor(mu_ai, mu_di, r_vector_i)              # orientation factor between molecules
+
+            forster_coupling += k_e * k**2 * np.dot(mu_ai, mu_di) / (n**2 * distance**3)
+
+    foster_data[hash_string] = forster_coupling                            # memory update for new couplings
+
     return forster_coupling
 
 
