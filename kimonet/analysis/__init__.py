fix length tensor calculation added example

kimonet/analysis/__init__.py
@@ -79,7 +79,7 @@ def plot_polar_plot(tensor_full, plane=(0, 1), title='', max=None, crystal_label
     theta = []
     for i in np.arange(0, np.pi*2, 0.01):
         unit_vector = np.array([np.cos(i), np.sin(i)])
-        r.append(np.sqrt(np.dot(unit_vector, np.dot(tensor, unit_vector))))  # <n|D|n>
+        r.append(np.dot(unit_vector, np.dot(tensor, unit_vector)))  # <n|D|n>
         theta.append(i)
 
     if max is None:

kimonet/analysis/trajectory_analysis.py
@@ -74,14 +74,13 @@ class TrajectoryAnalysis:
         dl_tensor_list = [traj.get_diffusion_length_square_tensor(state) for traj in self.trajectories
                           if not np.isnan(traj.get_diffusion_length_square_tensor(state)).any()]
 
-        tensor = np.abs(np.average(dl_tensor_list, axis=0))
+        tensor = np.average(dl_tensor_list, axis=0)
 
         if unit_cell is not None:
             trans_mat = normalize_cell(unit_cell)
             mat_inv = np.linalg.inv(trans_mat)
             # tensor = np.dot(mat_inv.T, tensor)
             tensor = np.dot(np.dot(mat_inv.T, tensor), mat_inv)
-
 
         return tensor
 

kimonet/system/vibrations.py
@@ -32,12 +32,13 @@ class MarcusModel:
 
     def get_vib_spectrum(self, target_state, origin_state):
 
-        #elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
+        # elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
         elec_trans_ene = target_state.energy - origin_state.energy
+        transition = Transition(target_state, origin_state, symmetric=False)
 
         temp = self.temperature  # temperature (K)
 
-        reorg_ene = np.sum(self.reorganization_energies[Transition(target_state, origin_state, symmetric=False)])
+        reorg_ene = np.sum(self.reorganization_energies[transition])
 
         sign = np.sign(elec_trans_ene)
 
@@ -82,9 +83,12 @@ class LevichJortnerModel:
     def set_state_energies(self, state_energies):
         self.state_energies = state_energies
 
-    def get_vib_spectrum(self, transition):
+    def get_vib_spectrum(self, target_state, origin_state):
 
-        elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
+        elec_trans_ene = target_state.energy - origin_state.energy
+        transition = Transition(target_state, origin_state, symmetric=False)
+
+        #elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
 
         temp = self.temperature  # temperature (K)
         ext_reorg_ene = self.external_reorganization_energies[transition]
@@ -142,7 +146,9 @@ class EmpiricalModel:
     def set_state_energies(self, state_energies):
         self.state_energies = state_energies
 
-    def get_vib_spectrum(self, transition):
+    def get_vib_spectrum(self, target_state, origin_state):
+        transition = Transition(target_state, origin_state, symmetric=False)
+
         # Temperature is not actually used. This is to keep common interface
         return self.empirical_function[transition]
 
@@ -171,7 +177,7 @@ class GaussianModel:
                      str(self.deviations),
                      str(self.reorganization_energies)))
 
-    def get_vib_spectrum(self, transition):
+    def get_vib_spectrum(self, target_state, origin_state):
 
         """
         :param donor: energy diference between states
@@ -179,7 +185,10 @@ class GaussianModel:
         :return: Franck-Condon-weighted density of states in gaussian aproximation
         """
 
-        elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
+        elec_trans_ene = target_state.energy - origin_state.energy
+        transition = Transition(target_state, origin_state, symmetric=False)
+
+        #elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
         reorg_ene = np.sum(self.reorganization_energies[transition])
 
         deviation = self.deviations[transition]     # atomic units
@@ -204,8 +213,12 @@ class NoVibration:
     def set_state_energies(self, state_energies):
         self.state_energies = state_energies
 
-    def get_vib_spectrum(self, transition):
-        elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
+    def get_vib_spectrum(self, target_state, origin_state):
+
+        elec_trans_ene = target_state.energy - origin_state.energy
+        transition = Transition(target_state, origin_state, symmetric=False)
+
+        # elec_trans_ene = self.state_energies[transition[1]] - self.state_energies[transition[0]]
 
         def vib_spectrum(e):
             if elec_trans_ene == e:

scripts/tetracene_simple.py
@@ -1,4 +1,4 @@
-# Tetracene exemaple
+# Tetracene exemaple based in data from ChemistryOpen 2016, 5,201-205
 from kimonet.system.generators import regular_system, crystal_system
 from kimonet.analysis import Trajectory, visualize_system, TrajectoryAnalysis, plot_polar_plot
 from kimonet import system_test_info
@@ -17,31 +17,7 @@ import numpy as np
 
 # states list
 s1 = State(label='s1', energy=2.9705, multiplicity=1, size=1)
-tt = State(label='tt', energy=2.0, multiplicity=1, size=2, connected_distance=8)
 t1 = State(label='t1', energy=1.5, multiplicity=3, size=1)
-
-
-# transition moments
-transition_moment = {Transition(s1, gs): [0.1, 0.0]}
-
-
-def direction_transfer(initial, final, rate_constant=1):
-    """
-    Allows transfer along a single direction
-
-    :param initial:
-    :param final:
-    :param rate_constant:
-    :return:
-    """
-
-    r = initial[0].get_center().get_coordinates() - initial[1].get_center().get_coordinates()
-    dot = np.dot(r, initial[0].get_center().get_orientation_vector())
-
-    #if np.abs(dot) > 0.1:
-    #    rate_constant = 0
-
-    return rate_constant
 
 
 def electronic_coupling_direction(initial, final, couplings=None):
@@ -65,15 +41,20 @@ def electronic_coupling_direction(initial, final, couplings=None):
     dot_ab_2 = np.abs(np.dot(r, [1, -1]))/np.linalg.norm([1, -1])/norm
 
     dot_b = np.abs(np.dot(r, [0, 1]))/np.linalg.norm([0, 1])/norm
+
+    #print('->', dot_a, dot_ab_1, dot_ab_1, dot_b)
+
     ichosen = int(np.argmax([dot_a, dot_ab_1, dot_ab_2, dot_b]))
+
+    # print('coup: ', ['a', 'ab', 'ab', 'b'][ichosen])
 
     return couplings[ichosen]  # eV
 
 
 # Electronic couplings in eV for the closest neighbor molecule in the indicated direction
 singlet_couplings = [12.61e-3,  # a
-                     41.85e-3,  # ab
-                     41.85e-3,  # ab
+                     41.85e-30,  # ab
+                     41.85e-30,  # ab
                      27.51e-3]  # b
 
 triplet_couplings = [0.0e-3,  # a
@@ -81,10 +62,10 @@ triplet_couplings = [0.0e-3,  # a
                      7.2e-3,  # ab
                      1.2e-3]  # b
 # Vibrations
-vibrational_model = MarcusModel(reorganization_energies={(gs, s1): 0.07,
-                                                         (s1, gs): 0.07,
-                                                         (gs, t1): 0.07,   # assuming triplet same reorganization
-                                                         (t1, gs): 0.07},  # energy as singlet
+vibrational_model = MarcusModel(reorganization_energies={(gs, s1): 0.185,
+                                                         (s1, gs): 0.185,
+                                                         (gs, t1): 0.165,   # assuming triplet same reorganization
+                                                         (t1, gs): 0.165},  # energy as singlet
                                 temperature=300)
 #################################################################################
 
@@ -95,15 +76,16 @@ system = crystal_system(molecules=[molecule, molecule],  # molecule to use as re
                         scaled_site_coordinates=[[0.0, 0.0],
                                                  [0.5, 0.5]],
                         unitcell=[[7.3347, 0.0000],
-                                  [-0.2242, 6.0167]],
-                        dimensions=[4, 4],  # supercell size
+                                  [-0.2242, 6.0167]], # -0.2242
+                        dimensions=[2, 2],  # supercell size
                         orientations=[[0.0, 0.0, np.pi/8],
                                       [0.0, 0.0, -np.pi/8]])  # if element is None then random, if list then Rx Ry Rz
 
 system.cutoff_radius = 8.1  # Angstroms
 
                          # Transport
-system.process_scheme = [GoldenRule(initial_states=(s1, gs), final_states=(gs, s1),
+system.process_scheme = [
+                         GoldenRule(initial_states=(s1, gs), final_states=(gs, s1),
                                     electronic_coupling_function=electronic_coupling_direction,
                                     arguments={'couplings': singlet_couplings},
                                     vibrations=vibrational_model,
@@ -113,55 +95,39 @@ system.process_scheme = [GoldenRule(initial_states=(s1, gs), final_states=(gs, s
                                     arguments={'couplings': triplet_couplings},
                                     vibrations=vibrational_model,
                                     description='triplet transport'),
-                         # Transitions
-                         SimpleRate(initial_states=(s1, gs), final_states=(tt,),
-                                    rate_constant=8.3,  # ns^-1
-                                    description='singlet fission'),
-                         SimpleRate(initial_states=(tt,), final_states=(gs, s1),
-                                    rate_constant=1.0,  # ns^-1
-                                    description='triplet fusion'),
-                         SimpleRate(initial_states=(tt,), final_states=(t1, t1),
-                                    rate_constant=2.0,  # ns^-1
-                                    description='triplet dissociation'),
                          # Decays
                          SimpleRate(initial_states=(s1,), final_states=(gs,),
-                                    rate_constant=8e-2,  # ns^-1
+                                    rate_constant=1/1370,
                                     description='Singlet decay '),
                          SimpleRate(initial_states=(t1,), final_states=(gs,),
-                                    rate_constant=1.6e-5,  # ns^-1
+                                    rate_constant=1/0.15,
                                     description='Triplet decay'),
-                         SimpleRate(initial_states=(t1, t1), final_states=(s1, gs),
-                                    rate_constant=1.8e-6,  # ns^-1
-                                    description='triplet-triplet annihilation'),
-                         #SimpleRate(initial_states=(s1, s1), final_states=(gs, gs),
-                         #           rate_constant=1,
-                         #           description='singlet-singlet annihilation'),
-                         #SimpleRate(initial_states=(t1, s1), final_states=(gs, gs),
-                         #           rate_constant=100,
-                         #           description='singlet-triplet annihilation'),
                          ]
 
 np.random.seed(0)
 
-system.add_excitation_random(s1, 2)
+system.add_excitation_random(s1, 1)
+#system.add_excitation_random(t1, 1)
 system_test_info(system)
 visualize_system(system)
 
-# exit()
-trajectories = calculate_kmc(system,
-                             num_trajectories=50,    # number of trajectories that will be simulated
-                             max_steps=1000,         # maximum number of steps for trajectory allowed
+
+#trajectories = calculate_kmc(system,
+trajectories = calculate_kmc_parallel_py2(system, processors=6,
+                             num_trajectories=100,    # number of trajectories that will be simulated
+                             max_steps=200,         # maximum number of steps for trajectory allowed
                              silent=False)
 
 
-store_trajectory_list(trajectories, 'singlet_fission.h5')
-#trajectories = load_trajectory_list('singlet_fission.h5')
+store_trajectory_list(trajectories, 'test_t.h5')
+
+#trajectories = load_trajectory_list('test.h5')
 
 analysis = TrajectoryAnalysis(trajectories)
 
-for s in ['s1', 't1']:
+for s in ['s1']:
     print('STATE: ', s)
-    print('diffusion coefficient (average): {} angs^2/ns'.format(analysis.diffusion_coefficient(s)))
+    print('diffusion coefficient: {} angs^2/ns'.format(analysis.diffusion_coefficient(s)))
     print('lifetime: {} ns'.format(analysis.lifetime(s)))
     print('diffusion length: {} angs'.format(analysis.diffusion_length(s)))
 
@@ -171,7 +137,6 @@ for s in ['s1', 't1']:
     print('diffusion length tensor')
     print(analysis.diffusion_length_square_tensor(s))
     print(analysis.diffusion_length_square_tensor(s, unit_cell=system.supercell))
-
     # print(np.sqrt(analysis.diffusion_coeff_tensor()*analysis.lifetime()*2))
 
     plt = analysis.plot_2d(state=s)
@@ -179,11 +144,16 @@ for s in ['s1', 't1']:
     analysis.plot_distances(state=s)
     plt.show()
 
-    plot_polar_plot(analysis.diffusion_coeff_tensor(s))
-    plot_polar_plot(analysis.diffusion_coeff_tensor(s, unit_cell=system.supercell))
+    analysis.plot_histogram()
+    plot_polar_plot(analysis.diffusion_coeff_tensor(s), title='Diff. coeff. tensor')
+    plot_polar_plot(analysis.diffusion_coeff_tensor(s, unit_cell=system.supercell),
+                    title='Diff. coeff. tensor', crystal_labels=True)
+    plot_polar_plot(analysis.diffusion_length_square_tensor(s), title='Length square tensor')
+    plot_polar_plot(analysis.diffusion_length_square_tensor(s, unit_cell=system.supercell),
+                    title='Length square tensor',
+                    crystal_labels=True)
 
-
-for s in ['s1', 't1', 'tt']:
-    analysis.plot_exciton_density(state=s)
 plt = analysis.plot_exciton_density()
+for s in ['s1']:
+    analysis.plot_exciton_density(state=s)
 plt.show()