Fixed bug when used multiple molecules Corrected error causing wrong calculation of FCWD integral Minor cleaning Added jupyter notebook example

examples/2d_simple.py
@@ -24,18 +24,31 @@ transitions = [Transition(s1, gs,
                           tdm=[0.1, 0.0],  # a.u.
                           reorganization_energy=0.08)] # eV
 
-molecule = Molecule()
 
-# physical conditions of the system
-conditions = {'refractive_index': 1}
 
 # define system as a crystal
-system = crystal_system(molecules=[molecule],  # molecule to use as reference
-                        scaled_site_coordinates=[[0.0, 0.0]],
+molecule = Molecule()
+
+#print(molecule, molecule.state, molecule.state.get_center())
+
+molecule2 = Molecule(site_energy=2)
+
+print(molecule2, molecule2.state, molecule2.state.get_center())
+print(molecule, molecule.state, molecule.state.get_center())
+
+
+system = crystal_system(molecules=[molecule, molecule],  # molecule to use as reference
+                        scaled_site_coordinates=[[0.0, 0.0],
+                                                 [0.0, 0.5]],
                         unitcell=[[5.0, 1.0],
                                   [1.0, 5.0]],
                         dimensions=[2, 2],  # supercell size
-                        orientations=[[0.0, 0.0, np.pi/2]])  # if element is None then random, if list then Rx Ry Rz
+                        orientations=[[0.0, 0.0, np.pi/2],
+                                      [0.0, 0.0, 0.0]])  # if element is None then random, if list then Rx Ry Rz
+
+print([m.site_energy for m in system.molecules])
+print(system.get_ground_states())
+
 
 # set initial exciton
 system.add_excitation_index(s1, 0)
@@ -64,7 +77,7 @@ visualize_system(system)
 # do the kinetic Monte Carlo simulation
 trajectories = calculate_kmc(system,
                              num_trajectories=5,    # number of trajectories that will be simulated
-                             max_steps=1000,         # maximum number of steps for trajectory allowed
+                             max_steps=100,         # maximum number of steps for trajectory allowed
                              silent=False)
 
 # specific trajectory plot
@@ -73,6 +86,10 @@ trajectories[0].plot_2d().show()
 
 # resulting trajectories analysis
 analysis = TrajectoryAnalysis(trajectories)
+
+print('diffusion coefficient: {:9.5e} Angs^2/ns'.format(analysis.diffusion_coefficient()))
+print('lifetime:              {:9.5e} ns'.format(analysis.lifetime()))
+print('diffusion length:      {:9.5e} Angs'.format(analysis.diffusion_length()))
 
 for state in analysis.get_states():
     print('\nState: {}\n--------------------------------'.format(state))

kimonet/analysis/trajectory_graph.py
@@ -284,7 +284,8 @@ class TrajectoryGraph:
             return self._coordinates_dict[state]
 
         if state is None:
-            self._coordinates_dict[state] = np.array([np.array(ar).T.tolist() for ar in self._vector_list_global()[0]])
+            self._coordinates_dict[state] = np.array([np.array(ar).T.tolist() for ar in self._vector_list_global()[0]],
+                                                     dtype=object).tolist()
             #print('--> none shape', np.array(self._coordinates_dict[state]).shape)
             #print(self._coordinates_dict[state][0])
             #print(self._coordinates_dict[state][1])

kimonet/core/processes/fcwd.py
@@ -4,7 +4,7 @@ from scipy.integrate import quad
 import math
 
 ###########################################################################################################
-#                                 Frank-Condon weighted density
+#                 Frank-Condon weighted density (OLD and deprecated)
 ###########################################################################################################
 overlap_data = {}
 

kimonet/core/processes/types.py
@@ -235,16 +235,16 @@ class GoldenRule(BaseProcess):
         def overlap(x):
             return donor_vib_dos(x) * acceptor_vib_dos(x)
 
-        d = BOLTZMANN_CONSTANT*1000*10
-        sup_range = np.max([self.initial[0].energy, self.initial[0].energy]) + d
-        inf_range = np.min([self.initial[0].energy, self.initial[0].energy]) - d
+        d = BOLTZMANN_CONSTANT*10000
+        sup_range = np.abs(self.initial[0].energy - self.initial[1].energy) + d
 
+        #if isinstance(self.vibrations, NoVibration):
+        #    # Treat donor_vib_dos & acceptor_vib_dos as delta functions
+        #    overlap_data[info] = 1.0 if donor_vib_dos - acceptor_vib_dos == 0 else 0.0
+        #else:
+        #    overlap_data[info] = quad(overlap, inf_range, sup_range, epsabs=1e-5, limit=1000)[0]
 
-        if isinstance(self.vibrations, NoVibration):
-            # Treat donor_vib_dos & acceptor_vib_dos as delta functions
-            overlap_data[info] = 1.0 if donor_vib_dos - acceptor_vib_dos == 0 else 0.0
-        else:
-            overlap_data[info] = quad(overlap, inf_range, sup_range, epsabs=1e-5, limit=1000)[0]
+        overlap_data[info] = quad(overlap, -sup_range, sup_range, epsabs=1e-5, limit=1000)[0]
 
         return overlap_data[info]
 
@@ -359,8 +359,8 @@ class InternalConversion(BaseProcess):
 
         vib_dos = self.vibrations.get_vib_spectrum(*transition)
 
-        if isinstance(self.vibrations, NoVibration):
-            return 1.0 if vib_dos == 0 else 0.0
+        #if isinstance(self.vibrations, NoVibration):
+        #    return 1.0 if vib_dos == 0 else 0.0
 
         return vib_dos(0)
 

kimonet/system/generators.py
@@ -71,13 +71,13 @@ def crystal_system(molecules,
 
     molecules_list = []                              # list of instances of class molecule
 
-    for i, (coordinate, molecule) in enumerate(zip(scaled_site_coordinates, molecules)):
+    for i, (coordinate, molecule_type) in enumerate(zip(scaled_site_coordinates, molecules)):
         for subset in itertools.product(*[list(range(n)) for n in dimensions]):
 
             r_cell = np.sum([s * lattice_vector for s, lattice_vector in zip(subset, unitcell)], axis=0)
             coor = r_cell + np.dot(unitcell.T, coordinate)
 
-            molecule = molecule.copy()  # copy of the generic instance
+            molecule = molecule_type.copy()  # copy of the generic instance
             molecule.set_coordinates(coor)
             molecule.name = 'a{}'.format(i+1)
 

kimonet/system/molecule.py
@@ -8,7 +8,7 @@ class Molecule(object):
 
     def __init__(self,
                  name=None,
-                 state=_GS_.copy(),
+                 state=None,
                  vdw_radius=1.0,  # Angstrom
                  coordinates=(0,),  # Angstrom
                  orientation=(0, 0, 0),  # Rx, Ry, Rz (radians)
@@ -24,8 +24,6 @@ class Molecule(object):
         # set state energies to vibrations
         #vibrations.set_state_energies(state_energies)
 
-        # self._state = self._labels_to_state[state]
-        self._state = state
         self._coordinates = np.array(coordinates)
         self.orientation = np.array(orientation)
         self._cell_state = np.zeros_like(coordinates, dtype=int)
@@ -33,8 +31,13 @@ class Molecule(object):
         self._site_energy = site_energy
         self.name = name
 
-        state.remove_molecules()
-        state.add_molecule(self)
+        if state is None:
+            self._state = _GS_.copy()
+        else:
+            self._state = state
+
+        self._state.remove_molecules()
+        self._state.add_molecule(self)
 
     def __hash__(self):
         return hash((

kimonet/system/state.py
@@ -124,8 +124,9 @@ class State(object):
     def vdw_radius(self):
         return np.average([mol.get_vdw_radius() for mol in self.get_molecules()])
 
-ground_state = State(label='gs', energy=0.0, multiplicity=1)
 
+# definition of ground state
+ground_state = State(label='gs', energy=0.0, multiplicity=1)
 
 if __name__ == '__main__':
     print('Test state')