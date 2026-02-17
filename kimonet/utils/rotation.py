8e6d6847c8f0e298826d9347b98e67d50b8418d0
Abel Carreras <abelcarreras83@gmail.com>
2026-02-17 10:51:27 +0100

    modify rotate_vector function. Add get_orientation_from_vectors helper function

--- kimonet/utils/rotation.py
+++ kimonet/utils/rotation.py
@@ -1,59 +1,140 @@
 import numpy as np
 
 
-def rot_x(angle):
-    """
-    rotate with respect x axis (right hand criteria)
-    :param angle:
-    :return:
-    """
-    return np.array([[1,             0,              0],
-                     [0, np.cos(angle), -np.sin(angle)],
-                     [0, np.sin(angle),  np.cos(angle)]])
-
-
-def rot_y(angle):
-    """
-    rotate with respect y axis (right hand criteria)
-    :param angle:
-    :return:
-    """
-    return np.array([[np.cos(angle),  0, np.sin(angle)],
-                     [0,              1,             0],
-                     [-np.sin(angle), 0, np.cos(angle)]])
-
-
-def rot_z(angle):
-    """
-    rotate with respect z axis (right hand criteria)
-    :param angle:
-    :return:
-    """
-    return np.array([[np.cos(angle), -np.sin(angle), 0],
-                     [np.sin(angle),  np.cos(angle), 0],
-                     [0,                          0, 1]])
-
-
-def rotate_vector_3d(vector, ang_x, ang_y, ang_z):
-    return np.dot(rot_z(ang_z), np.dot(rot_y(ang_y), np.dot(rot_x(ang_x), vector)))
-
-
 def rotate_vector(vector, orientation):
-    """
-    Rotate vector of dimensions (1-3)
+    """Rotate a vector of dimension 1-3 using effective Euler angles
 
-    :param vector: vector to rotate
-    :param orientation: angles (in radians) to rotate respect to axis x, y, z
+    :param vector: vector to be rotated
+    :param orientation: orientation list (1D: only rotation in X, 2D: only rotation in Z, 3D: Euler angles (RX RY RZ))
+
     :return: rotated vector
+
     """
 
-    norm = np.linalg.norm(vector)
     n_dim = len(vector)
-    ang_x, ang_y, ang_z = orientation
-    rotated_vector = np.dot(rot_z(ang_z)[:n_dim, :n_dim],
-                        np.dot(rot_y(ang_y)[:n_dim, :n_dim],
-                               np.dot(rot_x(ang_x)[:n_dim, :n_dim],
-                                      vector)))
+    norm = np.linalg.norm(vector)
+    rotated = vector.copy()
 
-    # Rotated vector is defined to have same norm as original whatever orientation
-    return rotated_vector/np.linalg.norm(rotated_vector) * norm
+    if n_dim == 1:
+        # 1D: flip sign if angle > pi/2
+        if orientation[0] % (2*np.pi) > np.pi/2:
+            rotated = -np.array(rotated)
+
+    elif n_dim == 2:
+        ang_z = orientation[2]
+        c, s = np.cos(ang_z), np.sin(ang_z)
+        R = np.array([[c, -s],
+                      [s,  c]])
+        rotated = R @ vector
+
+    elif n_dim == 3:
+        ang_x, ang_y, ang_z = orientation
+        Rx = np.array([[1, 0, 0],
+                       [0, np.cos(ang_x), -np.sin(ang_x)],
+                       [0, np.sin(ang_x),  np.cos(ang_x)]])
+        Ry = np.array([[ np.cos(ang_y), 0, np.sin(ang_y)],
+                       [0, 1, 0],
+                       [-np.sin(ang_y), 0, np.cos(ang_y)]])
+        Rz = np.array([[np.cos(ang_z), -np.sin(ang_z), 0],
+                       [np.sin(ang_z),  np.cos(ang_z), 0],
+                       [0, 0, 1]])
+        rotated = Rz @ Ry @ Rx @ vector
+
+    # normalize
+    return rotated / np.linalg.norm(rotated) * norm
+
+
+def get_orientation_from_vectors(v_reference, v_target):
+    """
+    get orientation angles that rotate a v_reference vector to a v_target vector
+
+    :param v_reference: reference vector
+    :param v_target: target vector
+    :return: orientation angles
+    """
+
+    # Compute Z-Y-X Euler angles to rotate v_init to v_final
+    v_reference = np.array(v_reference, dtype=float)/np.linalg.norm(v_reference)
+    v_target = np.array(v_target, dtype=float) / np.linalg.norm(v_target)
+
+    assert len(v_reference) == len(v_target)
+
+    n_dim = len(v_reference)
+
+    if n_dim == 1:
+        if n_dim == 1:
+            v_reference = v_reference / np.linalg.norm(v_reference)
+            v_target = v_target / np.linalg.norm(v_target)
+
+            # If vectors have same direction -> no rotation
+            if np.dot(v_reference, v_target) > 0:
+                angle = 0.0
+            else:
+                # Opposite direction -> flip (pi rotation)
+                angle = np.pi
+
+            return [angle, 0.0, 0.0]
+
+    if n_dim == 2:
+        angle = np.arctan2(v_reference[0] * v_target[1] - v_reference[1] * v_target[0],  # cross product (z-component)
+                           np.dot(v_reference, v_target))  # dot product
+
+        return np.array([0.0, 0.0, angle]).tolist()
+
+    elif n_dim == 3:
+        # Compute rotation axis and angle
+        axis = np.cross(v_reference, v_target)
+        sin_angle = np.linalg.norm(axis)
+        cos_angle = np.dot(v_reference, v_target)
+
+        if sin_angle < 1e-8:
+            # Vectors are colinear: no rotation or 180 deg
+            if cos_angle > 0:
+                return 0.0, 0.0, 0.0  # no rotation
+            else:
+                # 180 deg rotation around any perpendicular axis
+                axis = np.array([1, 0, 0]) if abs(v_reference[0]) < 0.9 else np.array([0, 1, 0])
+                axis /= np.linalg.norm(axis)
+                angle = np.pi
+        else:
+            axis /= sin_angle
+            angle = np.arctan2(sin_angle, cos_angle)
+
+        # Rodrigues formula to get rotation matrix
+        K = np.array([[0, -axis[2], axis[1]],
+                      [axis[2], 0, -axis[0]],
+                      [-axis[1], axis[0], 0]])
+        R = np.eye(3) + np.sin(angle) * K + (1 - np.cos(angle)) * (K @ K)
+
+        # Decompose R into Z-Y-X Euler angles
+        if abs(R[2, 0]) < 1.0:
+            ang_y = -np.arcsin(R[2, 0])
+            ang_x = np.arctan2(R[2, 1]/np.cos(ang_y), R[2, 2]/np.cos(ang_y))
+            ang_z = np.arctan2(R[1, 0]/np.cos(ang_y), R[0, 0]/np.cos(ang_y))
+        else:
+            # Gimbal lock: ang_y = ±pi/2
+            ang_y = np.pi/2 if R[2, 0] <= -1 else -np.pi/2
+            ang_x = 0
+            ang_z = np.arctan2(-R[0, 1], R[1, 1])
+
+        return np.array([ang_x, ang_y, ang_z]).tolist()
+
+    raise Exception('Wrong dimension, must be 1, 2 or 3')
+
+
+if __name__ == '__main__':
+    orientation = get_orientation_from_vectors([5, 0, 0], [1.0, -1.0, 0.0])
+    print('3D orientation: ', orientation)
+    vector = rotate_vector([5, 0, 0], orientation)
+    print('3D result: ', vector, np.linalg.norm(vector))
+
+    orientation = get_orientation_from_vectors([5, 0], [-2.0, 1.0])
+    print('2D orientation: ', orientation)
+    vector = rotate_vector([5, 0], orientation)
+    print('2D result: ', vector, np.linalg.norm(vector))
+
+    orientation = get_orientation_from_vectors([5], [-1.0])
+    print('1D orientation: ', orientation)
+    vector = rotate_vector([5], orientation)
+    print('1D result: ', vector, np.linalg.norm(vector))
+
